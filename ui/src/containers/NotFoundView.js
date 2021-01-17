3ef4af50b75be00acafd46c4b5a21ac4fdc1ca35
apehovski <apehovski@gmail.com>
2021-01-17 16:48:27 +0300

    UI: redirect from NotFound page

--- ui/src/containers/NotFoundView.js
+++ ui/src/containers/NotFoundView.js
@@ -1,13 +1,39 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, {useEffect, useState} from 'react';
+import {Link, useHistory} from 'react-router-dom';
+
+import styled from "styled-components";
+
+const StyledWrap = styled.div`
+  p {
+    text-align: center;
+  }
+`
 
 export default () => {
+  let history = useHistory();
+  const [count, setCount] = useState(5);
+
+  useEffect(() => {
+    if (count === 0) {
+      history.push("/")
+      return;
+    }
+
+    const timer = setTimeout(() => {
+      setCount(count - 1)
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, [count]);
+
   return (
-    <div>
-      <p style={{textAlign: "center"}}>
+    <StyledWrap>
+      <p>
         <span>Page does not exist. </span>
         <Link to="/">Go to Home</Link>
       </p>
-    </div>
+      <p>
+        <span>Or wait for redirect {count} seconds...</span>
+      </p>
+    </StyledWrap>
   )
 }
