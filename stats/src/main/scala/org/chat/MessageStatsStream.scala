626a22fcf78e9d10e49d5c400d0654bdd7a73a41
apehovski <apehovski@gmail.com>
2021-01-24 20:15:17 +0300

    Stats: create input topic if not exist

--- stats/src/main/scala/org/chat/MessageStatsStream.scala
+++ stats/src/main/scala/org/chat/MessageStatsStream.scala
@@ -4,11 +4,15 @@ import com.typesafe.config.ConfigFactory
 import com.typesafe.scalalogging.LazyLogging
 import io.circe.generic.auto._
 import io.circe.parser._
+import org.apache.kafka.clients.admin.{AdminClient, NewTopic}
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.common.errors.TopicExistsException
 import org.apache.kafka.streams.scala.StreamsBuilder
 import org.apache.kafka.streams.{KafkaStreams, StreamsConfig}
 
 import java.time.Duration
-import java.util.{Properties, UUID}
+import java.util.{Collections, Optional, Properties, UUID}
+import scala.util.Try
 
 
 object MessageStatsStream extends App with LazyLogging {
@@ -20,13 +24,22 @@ object MessageStatsStream extends App with LazyLogging {
 
   val conf = ConfigFactory.load
 
+  val clientId = "stats-app"
   val topicFrom = conf.getString("chat.topic.messages")
   val topicTo = conf.getString("chat.topic.stats")
   val kafkaHost = conf.getString("kafka.host")
 
-  val config: Properties = {
+  val clientConfig: Properties = {
     val p = new Properties()
-    p.put(StreamsConfig.APPLICATION_ID_CONFIG, "stats-app")
+    p.put(ProducerConfig.CLIENT_ID_CONFIG, clientId)
+    p.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaHost)
+    p
+  }
+  createTopic(topicFrom, clientConfig)
+
+  val streamConfig: Properties = {
+    val p = new Properties()
+    p.put(StreamsConfig.APPLICATION_ID_CONFIG, clientId)
     p.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaHost)
     p
   }
@@ -55,12 +68,24 @@ object MessageStatsStream extends App with LazyLogging {
   val topology = builder.build()
   logger.info("Topology: " + topology.describe())
 
-  val streams = new KafkaStreams(topology, config)
+  val streams = new KafkaStreams(topology, streamConfig)
   streams.cleanUp()
   streams.start()
 
   sys.ShutdownHookThread {
     streams.close(Duration.ofSeconds(10))
+  }
+
+  def createTopic(topic: String, config: Properties): Unit = {
+    val newTopic = new NewTopic(topic, Optional.empty[Integer](), Optional.empty[java.lang.Short]());
+    val adminClient = AdminClient.create(config)
+    Try (adminClient.createTopics(Collections.singletonList(newTopic)).all.get).recover {
+      case e: Exception =>
+        // Ignore if TopicExistsException, which may be valid if topic exists
+        if (!e.getCause.isInstanceOf[TopicExistsException]) throw new RuntimeException(e)
+    }
+    logger.info(s"Topic $topic created")
+    adminClient.close()
   }
 
 }
